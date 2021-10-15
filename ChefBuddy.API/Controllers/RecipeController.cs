8a2a7637617a4518127b6122d0e86b6c06fe1e52
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-14 22:10:04 -0400

    Added dummy endpoints and removed unused controller

---
+++ ChefBuddy.API/Controllers/RecipeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using ChefBuddy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChefBuddy.API.Controllers
+{
+    [ApiController]
+    [Route("recipe")]
+    public class RecipeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Recipe> GetAll()
+        {
+            return new List<Recipe>
+            {
+                new Recipe
+                {
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Id = Guid.NewGuid().ToString(),
+                    OwnerId = "Big Daddy 420",
+                    Steps = new List<Step>
+                    {
+                        new Step
+                        {
+                            Description = "Heat your oven to 400 degrees.",
+                            ImageUrl = "https://example.com/my-picture.jpg"
+                        },
+                        new Step
+                        {
+                            Description = "Sprinkle the spices",
+                            Ingredients = new List<Ingredient>
+                            {
+                                new Ingredient { Name = "Salt", Amount = "a pinch", Description = "Preferably sea salt" },
+                                new Ingredient { Name = "Ground black pepper", Amount = "a smidge" }
+                            }
+                        }
+                    }
+                },
+                new Recipe { Id = Guid.NewGuid().ToString() },
+                new Recipe { Id = Guid.NewGuid().ToString() }
+            };
+        }
+        
+        [HttpGet("id")]
+        public Recipe GetById(string id)
+        {
+            return new Recipe
+            {
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Id = Guid.NewGuid().ToString(),
+                OwnerId = "Big Daddy 420",
+                Steps = new List<Step>
+                {
+                    new Step
+                    {
+                        Description = "Heat your oven to 400 degrees.",
+                        ImageUrl = "https://example.com/my-picture.jpg"
+                    },
+                    new Step
+                    {
+                        IsOptional = true,
+                        Description = "Sprinkle the spices",
+                        Ingredients = new List<Ingredient>
+                        {
+                            new Ingredient { Name = "Salt", Amount = "a pinch", Description = "Preferably sea salt" },
+                            new Ingredient { Name = "Ground black pepper", Amount = "a smidge" }
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
