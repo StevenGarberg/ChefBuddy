4b087a005c28ad3149ab8af2c1479a879ea226fe
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-22 18:49:41 -0400

    Created recipe repo and service

---
+++ ChefBuddy.App/Repositories/RecipeRepository.cs
@@ -0,0 +1,64 @@
+﻿using ChefBuddy.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace ChefBuddy.App.Repositories;
+
+public class RecipeRepository
+{
+    private readonly IMongoClient _mongoClient;
+
+    public RecipeRepository(IMongoClient mongoClient)
+    {
+        _mongoClient = mongoClient;
+    }
+
+    public async Task<Recipe> CreateAsync(Recipe Recipe)
+    {
+        Recipe.Id = Guid.NewGuid().ToString();
+        Recipe.CreatedAt = DateTime.UtcNow;
+        Recipe.UpdatedAt = Recipe.CreatedAt;
+        await GetCollection().InsertOneAsync(Recipe);
+        return Recipe;
+    }
+        
+    public async Task<Recipe> Update(string id, Recipe data)
+    {
+        data.UpdatedAt = DateTime.UtcNow;
+        await GetCollection().ReplaceOneAsync(x => x.Id == id, data);
+        return data;
+    }
+        
+    public async Task Delete(string id)
+    {
+        await GetCollection().UpdateOneAsync(x => x.Id == id,
+            Builders<Recipe>.Update.Set(x => x.Deleted, true));
+    }
+
+    public async Task<Recipe> GetByIdAsync(string id)
+    {
+        return await GetCollection().AsQueryable()
+            .Where(x => x.Id == id)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Recipe>> GetAll()
+    {
+        return await GetCollection().AsQueryable()
+            .ToListAsync();
+    }
+        
+    public async Task<List<Recipe>> GetAllByOwnerId(string ownerId)
+    {
+        return await GetCollection().AsQueryable()
+            .Where(w => w.OwnerId == ownerId && !w.Deleted)
+            .ToListAsync();
+    }
+        
+    private IMongoCollection<Recipe> GetCollection()
+    {
+        IMongoDatabase database = _mongoClient.GetDatabase("ChefBuddy");
+        IMongoCollection<Recipe> collection = database.GetCollection<Recipe>("Recipes");
+        return collection;
+    }
+}
---
+++ ChefBuddy.App/Services/RecipeService.cs
@@ -0,0 +1,49 @@
+﻿using ChefBuddy.App.Repositories;
+using ChefBuddy.Models;
+
+namespace ChefBuddy.App.Services;
+
+public class RecipeService
+{
+    private readonly RecipeRepository _RecipeRepository;
+        
+    public RecipeService(RecipeRepository RecipeRepository)
+    {
+        _RecipeRepository = RecipeRepository;
+    }
+        
+    public async Task<Recipe> CreateAsync(string ownerId, Recipe Recipe)
+    {
+        if (Recipe == null)
+            throw new ArgumentNullException(nameof(Recipe));
+            
+        Recipe.OwnerId = ownerId;
+            
+        return await _RecipeRepository.CreateAsync(Recipe);
+    }
+
+    public async Task<Recipe> Update(string id, Recipe Recipe)
+    {
+        return await _RecipeRepository.Update(id, Recipe);
+    }
+        
+    public async Task Delete(string id)
+    {
+        await _RecipeRepository.Delete(id);
+    }
+
+    public async Task<Recipe> GetById(string id)
+    {
+        return await _RecipeRepository.GetByIdAsync(id);
+    }
+
+    public async Task<List<Recipe>> GetAllByOwnerId(string ownerId)
+    {
+        return await _RecipeRepository.GetAllByOwnerId(ownerId);
+    }
+
+    public async Task<List<Recipe>> GetAll()
+    {
+        return await _RecipeRepository.GetAll();
+    }
+}
