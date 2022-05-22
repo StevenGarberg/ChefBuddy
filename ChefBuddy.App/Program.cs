10916366a70412979abff226c9a7a4dd7639fca4
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-22 18:49:31 -0400

    App configuration and layout

--- ChefBuddy.App/Program.cs
+++ ChefBuddy.App/Program.cs
@@ -1,6 +1,9 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using ChefBuddy.App.Data;
+using ChefBuddy.App.Repositories;
+using ChefBuddy.App.Services;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+var conventionPack = new  ConventionPack {new CamelCaseElementNameConvention()};
+ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+var mongoDbConnectionString = builder.Configuration["ConnectionStrings:Mongo"];
+builder.Services.AddScoped<IMongoClient, MongoClient>(_ => new MongoClient(MongoClientSettings.FromConnectionString(mongoDbConnectionString)));
+
+// Radzen components
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
+// Repositories
+builder.Services.AddScoped<RecipeRepository>();
+
+// Services
+builder.Services.AddScoped<RecipeService>();
 
 var app = builder.Build();
 
